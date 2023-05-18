e6bf086f80330b98587529dc2367ed00dfe3abab
tiago <sawadeekrap1@gmail.com>
2023-05-18 01:48:13 -0300

    execimob

---
+++ app/Http/Controllers/PropertyController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class PropertyController extends Controller
+{
+    public function index()
+    {
+        $properties = DB::select("SELECT * FROM properties");
+        return view('property.index')->with('properties', $properties);
+    }
+
+    public function create()
+    {
+        return view('property.create');
+    }
+
+    public function store(Request $request)
+    {
+        var_dump($request);
+    }
+}
---
+++ resources/views/property/index.php
@@ -0,0 +1,32 @@
+<h1>Listagem de Produtos</h1>
+<p><a href="<?= url('/imoveis/novo') ?>">Cadastre um novo imovel</a></p>
+
+<?php
+
+    if(!empty($properties)){
+
+        echo "<table>";
+
+            echo "<tr>
+                    <td>Titulo</td>
+                    <td>Valor de Aluguel</td>
+                    <td>Valor de Compra</td>
+                 </tr>";
+
+
+        foreach($properties as $property){
+
+
+
+            echo "<tr>
+                    <td>{$property->title}</td>
+                    <td>R$ ". number_format($property->rental_price, 2, ',', '.')."</td>
+                    <td>R$ ". number_format($property->sale_price, 2, ',', '.')."</td>
+                 </tr>";
+
+        }
+
+
+            echo "</table>";
+
+    }
